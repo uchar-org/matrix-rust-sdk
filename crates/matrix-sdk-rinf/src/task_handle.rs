fe6dd20e7ae5dcb7612faa4a7ad18e92c42f57d0
Shakhzod Kudratov <shakhzodkudratov@gmail.com>
2025-08-30 17:43:58 +0500

    feat: copied bindings/ffi into crates/rinf and cleaned up uniffi bindings

---
+++ crates/matrix-sdk-rinf/src/task_handle.rs
@@ -0,0 +1,37 @@
+use matrix_sdk_common::executor::JoinHandle;
+use tracing::debug;
+
+/// A task handle is a way to keep the handle a task running by itself in
+/// detached mode.
+///
+/// It's a thin wrapper around [`JoinHandle`].
+pub struct TaskHandle {
+    handle: JoinHandle<()>,
+}
+
+impl TaskHandle {
+    // Create a new task handle.
+    pub fn new(handle: JoinHandle<()>) -> Self {
+        Self { handle }
+    }
+}
+
+impl TaskHandle {
+    // Cancel a task handle.
+    pub fn cancel(&self) {
+        debug!("Cancelling the task handle");
+
+        self.handle.abort();
+    }
+
+    /// Check whether the handle is finished.
+    pub fn is_finished(&self) -> bool {
+        self.handle.is_finished()
+    }
+}
+
+impl Drop for TaskHandle {
+    fn drop(&mut self) {
+        self.cancel();
+    }
+}
